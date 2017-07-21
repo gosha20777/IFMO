8bb7d9ad03ad53084b5059d3e0aa83a6a0ea47ef
Georgy Perevozchikov <gosha20777@live.ru>
2017-07-22 00:21:15 +0200

    Home Work 5 TripleExpression-FIX
    TripleExpression - Fixed for C#

---
+++ 05/CSharp/TripleExpression_Classes_ONLY/Add.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripleExpressionApp
+{
+    public class Add : BinaryOperator
+    {
+        public Add(ITripleExpression first, ITripleExpression second)
+        {
+            fOp = first;
+            sOp = second;
+        }
+        
+        protected override double Apply(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
